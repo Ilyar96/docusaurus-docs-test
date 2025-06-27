9fcf1f11645b36f5ba2863e9619ad89bda517070
Ilyar Kassymov <kasyymov.ilyar@gmail.com>
2025-06-27 09:18:53 +0300

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,11 @@ const config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://github.com/",
+	// url: "https://github.com/",
+	url: "https://docusaurus.iljar96.ru",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/docusaurus-docs-test/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
