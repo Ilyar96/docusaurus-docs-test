286ac890a0a4f291fb824d950fbc22e2ad8aca04
Ilyar Kassymov <kasyymov.ilyar@gmail.com>
2025-06-27 10:34:28 +0300

    Обновление конфигурации Docusaurus для использования переменных окружения и улучшение процесса развертывания в GitHub Actions.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://github.com/",
+	url: process.env.DOCUSAURUS_URL || "https://github.com/",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/docusaurus-docs-test/",
+	baseUrl: process.env.DOCUSAURUS_BASEURL || "/docusaurus-docs-test/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
